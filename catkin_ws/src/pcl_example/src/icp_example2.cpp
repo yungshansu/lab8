8d3fac7fca26a4f88fce7b6b376914c8b2e48396
yungshansu <michael1994822@gmail.com>
2017-11-21 18:37:57 +0000

    answer

--- catkin_ws/src/pcl_example/src/icp_example2.cpp
+++ catkin_ws/src/pcl_example/src/icp_example2.cpp
@@ -43,14 +43,15 @@ PointCloudXYZRGB::Ptr model_icp_align (new PointCloudXYZRGB);
 void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {	
 	//Step 1: Convert pointcloud type from Ros pointcloud2  (input) to PCL pointCLoudXYZRGB (model_t1)
-
+	pcl::fromROSMsg (*input, *model_t1);
 	//Step 2: Use function icp_pose_estimation() to register consequently moving object (model_t0, model_t1) 
 	//Step 3: Use function pose_publish() to publish moving object and aligned object pointcloud
 	if (model_t0->points.size() == model_t1->points.size()){
-
-
+		icp_pose_estimation (model_t0, model_t1);
+		pose_publish (model_icp_align,model_t0, model_t1);
  	}
-	//Step 4: Update the current point (model_t0)
+	//Step 4: Update the current point (model_t0)	
+ 	copyPointCloud(*model_t1,*model_t0);
 
 }   
 
